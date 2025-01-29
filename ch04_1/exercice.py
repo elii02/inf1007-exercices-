8bb95385689593ffcb36ed1ba91e2f8e3a824cd2
Elisabeth KJ <elisabeth.kerr-jarrold@polymtl.ca>
2025-01-29 17:19:00 -0500

    exo ch04_1 en classe faits

--- ch04_1/exercice.py
+++ ch04_1/exercice.py
@@ -3,22 +3,42 @@
 
 
 def is_even_len(string: str) -> bool:
+    chaine = "hello"
+    print("Chaine a un nrb pair" if len(chaine)%2 ==0 else "Chaine a un nbr impair")
     pass
 
 
 def remove_third_char(string: str) -> str:
+    chaine = "hello"
+    print(chaine[:2] + chaine[3:])
     pass
 
 
 def replace_char(string: str, old_char: str, new_char: str) -> str:
+    chaine='hello'
+    print(chaine.replace("o", "a"))
+    # chaine='hello'
+    # pos1 = chaine.find("o")
+    # print(chaine[:pos1] + "a" + chaine[:pos1 +1 :] )
+    # pos1 = chaine.rfind("o")
+    # print(chaine[:pos1] + "a" + chaine[:pos1 +1 :] )
     pass
 
 
 def get_number_of_char(string: str, char: str) -> int:
+    chaine='hello'
+    for car in chaine:
+        print(car)
+    # for i in range(len(chaine)):
+    #     print(i, chaine[1])
+    # for i, car in enumerate(chaine, 10):
+    #     print(i, car)
     pass
 
 
 def get_number_of_words(sentence: str, word: str) -> int:
+    paragraphe = """ Lorem ha ha je suis la plus belle hihi ou hi"""
+    print(len(paragraphe.split()))
     pass
 
 
