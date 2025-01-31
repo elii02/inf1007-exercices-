b356fbd09068858be740ebe74c432c0ee9649b42
Elisabeth KJ <elisabeth.kerr-jarrold@polymtl.ca>
2025-01-31 17:10:55 -0500

    ch4_02 3exos left

--- ch04_2/exercice.py
+++ ch04_2/exercice.py
@@ -5,15 +5,38 @@ import random
 
 
 def get_first_part_of_name(name):
-	return ""
+	name = name.split("-")
+	first_name= name[0]
+	first_name = first_name[0].upper() + first_name[1:].lower()
+	return "Bonjour " + first_name
 
 def get_random_sentence(animals, adjectives, fruits):
+	animaux = (chevreuil, chien, pigeon)
+	adjectifs = (rouge, officiel, lourd) 
+	fruits = (pommes, kiwis, bananes)
+
 	return ""
 
 def format_date(year, month, day, hours, minutes, seconds):
 	return ""
 
 def encrypt(text, shift):
+	# car = "Z"
+	# pas = 3
+	# print((ord(car) - ord("A")+1 + pas) %26)
+
+	phrase = "Allo"
+	car = "Z"
+	pas = 3
+	chaine = ""
+	for car in phrase: 
+		if "A" <= car <= "Z":
+			chaine += chr((ord(car) - ord("A") + pas) % 26 + ord("A"))
+		elif "a" <= car <= "z":
+			chaine += chr((ord(car) - ord("a") + pas) % 26 + ord("a"))
+		else:
+			chaine += car 
+	print(chaine)
 	return ""
 
 def decrypt(encrypted_text, shift):
